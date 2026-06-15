e8ada6347b463d12376be8a5daf1487f16a2e6f9
vitor-svg <zacariasmusun4@gmail.com>
2026-06-15 12:51:52 -0300

    v1.3.0 - sistema de vitória e derrota

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const palavra = "CASA";
 let letrasCorretas = [];
 let erros = 0;
+let jogoFinalizado = false;
 
 // Mostra a palavra escondida ao carregar a página
 mostrarPalavra();
@@ -20,22 +21,53 @@ function mostrarPalavra() {
     }
 
     document.getElementById("palavra").innerHTML = resultado;
+
+    verificarVitoria();
+}
+
+function verificarVitoria() {
+
+    let venceu = true;
+
+    for (let i = 0; i < palavra.length; i++) {
+
+        if (!letrasCorretas.includes(palavra[i])) {
+            venceu = false;
+        }
+
+    }
+
+    if (venceu) {
+
+        document.getElementById("mensagem").innerHTML =
+        "🎉 Você venceu!";
+
+        jogoFinalizado = true;
+    }
 }
 
 function jogar() {
 
+    if (jogoFinalizado) {
+        return;
+    }
+
     let letra = document.getElementById("letra").value.toUpperCase();
 
     if (letra === "") {
+
         document.getElementById("mensagem").innerHTML =
         "Digite uma letra!";
+
         return;
     }
 
     if (palavra.includes(letra)) {
 
         if (!letrasCorretas.includes(letra)) {
+
             letrasCorretas.push(letra);
+
         }
 
         document.getElementById("mensagem").innerHTML =
@@ -51,6 +83,13 @@ function jogar() {
         document.getElementById("erros").innerHTML =
         "Erros: " + erros + "/6";
 
+        if (erros >= 6) {
+
+            document.getElementById("mensagem").innerHTML =
+            "💀 Você perdeu!";
+
+            jogoFinalizado = true;
+        }
     }
 
     mostrarPalavra();
