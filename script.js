574dd66d73734c3380261ee7d34fdeb24b7c8db3
vitor-svg <zacariasmusun4@gmail.com>
2026-06-15 12:35:37 -0300

    v1.1.0 - exibição da palavra e letras corretas

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 const palavra = "CASA";
+let letrasCorretas = [];
 
-function jogar(){
-    let letra = document.getElementById("letra").value;
+// Mostra a palavra escondida ao carregar a página
+mostrarPalavra();
 
-    if(palavra.includes(letra.toUpperCase())){
+function mostrarPalavra() {
+    let resultado = "";
+
+    for (let i = 0; i < palavra.length; i++) {
+
+        if (letrasCorretas.includes(palavra[i])) {
+            resultado += palavra[i] + " ";
+        } else {
+            resultado += "_ ";
+        }
+
+    }
+
+    document.getElementById("palavra").innerHTML = resultado;
+}
+
+function jogar() {
+
+    let letra = document.getElementById("letra").value.toUpperCase();
+
+    if (letra === "") {
         document.getElementById("mensagem").innerHTML =
-        "A letra existe na palavra!";
-    }else{
+        "Digite uma letra!";
+        return;
+    }
+
+    if (palavra.includes(letra)) {
+
+        if (!letrasCorretas.includes(letra)) {
+            letrasCorretas.push(letra);
+        }
+
+        document.getElementById("mensagem").innerHTML =
+        "Você acertou uma letra!";
+
+    } else {
+
         document.getElementById("mensagem").innerHTML =
         "A letra não existe na palavra!";
+
     }
+
+    mostrarPalavra();
+
+    document.getElementById("letra").value = "";
 }
